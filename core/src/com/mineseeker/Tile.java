aac5f2b281edc92b194d471497e10027ccae0a85
Dennis <develop@coffeeden.org>
2022-05-12 15:53:27 -0400

    add new methods

--- core/src/com/mineseeker/Tile.java
+++ core/src/com/mineseeker/Tile.java
@@ -30,16 +30,16 @@ public class Tile {
         revealedTex = tex;
         testSprite = new Sprite(overlayTex);
         tileSprite = new SpriteBatch();
-        testSprite.setPosition(tile.getX(),tile.getY());
+        testSprite.setPosition(tile.getX(), tile.getY());
         testSprite.setOrigin(0, 0);
         tileSprite.setProjectionMatrix(camera.combined);
         testSprite.flip(false, true);
-        this.camera=camera;
+        this.camera = camera;
     }
 
     public void debugTile() {
-        System.out.println("Rectangle X,Y "+tile.getX()+" "+tile.getY());
-        System.out.println("Sprite X,Y "+testSprite.getX()+" "+testSprite.getY());
+        System.out.println("Rectangle X,Y " + tile.getX() + " " + tile.getY());
+        System.out.println("Sprite X,Y " + testSprite.getX() + " " + testSprite.getY());
     }
 
     public void draw() {
@@ -52,27 +52,74 @@ public class Tile {
         testSprite.draw(tileSprite);
         if (revealed) {
             testSprite.setTexture(revealedTex);
-        } else
-            testSprite.setTexture(overlayTex);
+        } else testSprite.setTexture(overlayTex);
         //tileSprite.draw(overlayTex, tile.x, tile.y);
 
         tileSprite.end();
         checkClicked();
-
     }
 
     public void checkClicked() {
-        if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT) &&
-                (Gdx.input.getX() >= tile.getX() &&
-                        Gdx.input.getX() <= tile.getX() + tile.getWidth()
-                        && Gdx.input.getY() >= tile.getY() &&
-                        Gdx.input.getY() <= tile.getY() + tile.getHeight())
-        ) {
+        if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT) && (Gdx.input.getX() >= tile.getX() && Gdx.input.getX() <= tile.getX() + tile.getWidth() && Gdx.input.getY() >= tile.getY() && Gdx.input.getY() <= tile.getY() + tile.getHeight())) {
             revealed = true;
             debugTile();
         }
-        if (Gdx.input.isButtonPressed(Input.Buttons.RIGHT))
-            revealed = false;
+        if (Gdx.input.isButtonPressed(Input.Buttons.RIGHT)) revealed = false;
+    }
+
+    public boolean isBomb() {
+        return isBomb;
+    }
+
+    public void setBomb(boolean bomb) {
+        isBomb = bomb;
+        setRevealedTex(new Texture("BombTile.PNG"));
+    }
+
+    public int getBombsAroundCount() {
+        return bombsAroundCount;
+    }
+
+    public void setBombsAroundCount(int bombsAroundCount) {
+        this.bombsAroundCount = bombsAroundCount;
+        if (!isBomb) {
+            switch (bombsAroundCount) {
+                case 0:
+                    break;
+                case 1:
+                    setRevealedTex(new Texture("Tile1.PNG"));
+                    break;
+                case 2:
+                    setRevealedTex(new Texture("Tile2.PNG"));
+                    break;
+                case 3:
+                    setRevealedTex(new Texture("Tile3.PNG"));
+                    break;
+                case 4:
+                    setRevealedTex(new Texture("Tile4.PNG"));
+                    break;
+                case 5:
+                    setRevealedTex(new Texture("Tile5.PNG"));
+                    break;
+                case 6:
+                    setRevealedTex(new Texture("Tile6.PNG"));
+                    break;
+                case 7:
+                    setRevealedTex(new Texture("Tile7.PNG"));
+                    break;
+                case 8:
+                    setRevealedTex(new Texture("Tile8.PNG"));
+                    break;
+            }
+        }
+    }
+
+    public String getRevealedTex() {
+        return revealedTex.toString(); //todo: this should return name of texture? or return an enum value?
+    }
+
+    public void setRevealedTex(Texture revealedTex) {
+        this.revealedTex = revealedTex;
     }
 
     public float getHeight() {
