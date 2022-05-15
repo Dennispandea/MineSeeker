4530781227092bdeb1f45e63bf4caa1fce2ad54a
Dennis <develop@coffeeden.org>
2022-05-15 03:07:25 -0400

    Make debug mode more robust, and togglable

--- core/src/com/mineseeker/MineSeeker.java
+++ core/src/com/mineseeker/MineSeeker.java
@@ -2,10 +2,14 @@ package com.mineseeker;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.utils.ScreenUtils;
+
+import java.text.DecimalFormat;
 
 public class MineSeeker extends ApplicationAdapter {
     public static int WIDTH = 600;
@@ -16,21 +20,38 @@ public class MineSeeker extends ApplicationAdapter {
     GameBoard gameBoard;
     BitmapFont font;
     SpriteBatch batch;
+    StringBuilder debugString;
     private OrthographicCamera camera;
+    boolean debugMode = false;
 
 
     @Override
     public void create() {
         //batch = new SpriteBatch();
         //img = new Texture("badlogic.jpg");
+        //tileTest = new Tile(64, 64, 0, 0, new Texture("BombTile.PNG"), camera);
+        cameraInit();
+        debugInit();
+        gameboardInit();
+
+    }
+
+    private void cameraInit() {
         camera = new OrthographicCamera(WIDTH, HEIGHT);
         camera.setToOrtho(true, WIDTH, HEIGHT);
+    }
+
+    private void gameboardInit() {
+        gameBoard = new GameBoard(16, 16, 40, camera);
+        gameBoard.create();
+    }
+
+    private void debugInit() {
         font = new BitmapFont(true);
+        font.setColor(1, 0, 0, 1);
         batch = new SpriteBatch();
         batch.setProjectionMatrix(camera.combined);
-        //tileTest = new Tile(64, 64, 0, 0, new Texture("BombTile.PNG"), camera);
-        gameBoard = new GameBoard(18, 18, 23, camera);
-        gameBoard.create();
+
 
     }
 
@@ -45,9 +66,43 @@ public class MineSeeker extends ApplicationAdapter {
 
         batch.begin();
         gameBoard.render();
-        font.draw(batch, Gdx.input.getX() + " " + Gdx.input.getY(), 64, HEIGHT-64);
+        debugModeCheck();
         batch.end();
 //        batch.end();
+    }
+
+
+    public String debugUpdate() {
+        debugString = new StringBuilder();
+        debugString.append("X:");
+        debugString.append(Gdx.input.getX());
+        debugString.append("   ");
+        debugString.append("Y:");
+        debugString.append(Gdx.input.getY());
+        debugString.append('\n');
+
+        debugString.append("FPS: ");
+        debugString.append(Gdx.graphics.getFramesPerSecond());
+        debugString.append(" Memory usage: ");
+        debugString.append(util.memUsageCalc(Gdx.app.getJavaHeap()));
+        debugString.append('\n');
+
+        debugString.append("Window Size: ");
+        debugString.append(Gdx.graphics.getWidth());
+        debugString.append(" ");
+        debugString.append(Gdx.graphics.getHeight());
+
+        return debugString.toString();
+    }
+
+    private void debugModeCheck() {
+        if (debugMode) {
+            font.draw(batch, debugUpdate(), 10, 10);
+        }
+        if (Gdx.input.isKeyJustPressed(Input.Keys.F3)) {
+            debugMode = !debugMode;
+            System.out.println("Toggled debug");
+        }
     }
 
     @Override
