018978ca0282c141d7b5a91242e576390feb692c
Dennis <develop@coffeeden.org>
2022-05-17 21:16:54 -0400

    remove the ugly sout

--- core/src/com/mineseeker/Tile.java
+++ core/src/com/mineseeker/Tile.java
@@ -100,20 +100,15 @@ public class Tile {
 
             if (flagged) {
                 scoreboard.removeUsedFlag();
-                System.out.println(flagged);
                 flagged = false;
-                System.out.println(flagged);
 
                 if (isBomb)
                     scoreboard.removeFlaggedBomb();
             }
-
              else if (!flagged){
                 if (scoreboard.getUsedFlags() < MineSeeker.TOTAL_MINES) {
                     scoreboard.addUsedFlag();
-                    System.out.println(flagged);
                     flagged = true;
-                    System.out.println(flagged);
 
                     if (isBomb)
                         scoreboard.addFlaggedBomb();
