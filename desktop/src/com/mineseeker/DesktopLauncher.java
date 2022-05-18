b654ff8231075b7d54e9e572d197fe919a46d25d
Dennis <develop@coffeeden.org>
2022-05-17 22:28:27 -0400

    move that to a more reasonable place

--- desktop/src/com/mineseeker/DesktopLauncher.java
+++ desktop/src/com/mineseeker/DesktopLauncher.java
@@ -14,10 +14,10 @@ public class DesktopLauncher {
     public static void main(String[] arg) {
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
         config.setWindowedMode(WIDTH, HEIGHT);
+        config.setResizable(false);
         config.useVsync(true);
         config.setForegroundFPS(60);
         config.setTitle("MineSeeker");
-        config.setResizable(false);
         new Lwjgl3Application(new MineSeeker(), config);
     }
 }
